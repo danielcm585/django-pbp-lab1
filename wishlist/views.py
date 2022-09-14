ea493f139ba2288f9f44b69d9955086d2768d37b
danielcm585 <danielcm585@gmail.com>
2022-09-14 20:46:57 +0700

    Create handler for json and xml request

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from wishlist.models import BarangWishlist
 
 def show_wishlist(request):
@@ -8,3 +10,20 @@ def show_wishlist(request):
         'nama': 'Kak Cinoy'
     }
     return render(request, "wishlist.html", context)
+
+def get_all_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize('xml',data), content_type='application/xml')
+
+def get_all_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize('json',data), content_type='application/json')
+
+def get_one_xml(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize('xml',data), content_type='application/xml')
+
+def get_one_json(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize('json',data), content_type='application/json')
+
