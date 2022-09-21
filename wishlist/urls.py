1535de577fc3e20c7a21ae0b4a2d2d47d24e4e0e
danielcm585 <danielcm585@gmail.com>
2022-09-21 22:49:00 +0700

    Commit lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import get_all_json, get_all_xml, get_one_json, get_one_xml, show_wishlist
+from wishlist.views import *
 
 app_name = 'wishlist'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('json/', get_all_json, name='get_all_json'),
     path('xml/<int:id>', get_one_xml, name='get_one_xml'),
     path('json/<int:id>', get_one_json, name='get_one_json'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
