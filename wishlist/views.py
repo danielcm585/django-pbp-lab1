08307729160bfb26ecabd0f4504324171852bf86
danielcm585 <danielcm585@gmail.com>
2022-09-28 21:40:30 +0700

    Create navbar

--- wishlist/views.py
+++ wishlist/views.py
@@ -14,23 +14,27 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
         'list_barang': data_barang_wishlist,
-        'nama': 'Kak Cinoy',
+        'nama': 'Daniel Christian Mandolang',
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
 
+@login_required(login_url='/wishlist/login')
 def get_all_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize('xml',data), content_type='application/xml')
 
+@login_required(login_url='/wishlist/login')
 def get_all_json(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize('json',data), content_type='application/json')
 
+@login_required(login_url='/wishlist/login')
 def get_one_xml(request, id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize('xml',data), content_type='application/xml')
 
+@login_required(login_url='/wishlist/login')
 def get_one_json(request, id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize('json',data), content_type='application/json')
