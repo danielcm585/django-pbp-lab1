e110a9a4c9c054e1e8906d69f442895ea5bc1376
danielcm585 <danielcm585@gmail.com>
2022-09-29 13:54:23 +0700

    Put navbar in all pages

--- example_app/views.py
+++ example_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'last_login': request.COOKIES['last_login']
+    }
+    return render(request, 'index.html', context)
